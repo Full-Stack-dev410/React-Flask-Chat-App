7df88b9be9cc8bf4d5cf0fb92ab2c6b1899cb9ec
Full stack developer <67923774+Full-Stack-dev410@users.noreply.github.com>
2020-07-24 04:49:10 +0900

    adf

--- index.py
+++ index.py
@@ -5,13 +5,13 @@ APP_DIR = os.path.abspath(os.path.dirname(__file__))
 STATIC_FOLDER = os.path.join(APP_DIR, '/build') # Where your webpack build output folder is
 TEMPLATE_FOLDER = os.path.join(APP_DIR, '/build') # Where your index.html file is located
 app = Flask(__name__, static_folder=STATIC_FOLDER, template_folder=TEMPLATE_FOLDER, static_url_path='/')
-app.debug = False
+# app.debug = True
 app.config['SECRET_KEY'] = 'mysecret'
 
 socketIo = SocketIO(app, cors_allowed_origins="*")
 
-# app.debug = True
-# app.host = 'localhost'git 
+app.debug = True
+app.host = 'localhost' 
 @socketIo.on("message")
 def handleMessage(msg):
     print(msg)
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
-let endPoint = "http://localhost:5000";
-let socket = io.connect(`${endPoint}`);
+// let endPoint = "http://localhost:5000";
+let socket = io.connect('/');
 
 const App = () => {
   const [messages, setMessages] = useState(["Hello And Welcome"]);
