dd37835f5a9c95e5bc165363ec01193a432fea4c
Full stack developer <67923774+Full-Stack-dev410@users.noreply.github.com>
2020-07-24 02:20:51 +0900

    second

--- index.py
+++ index.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify, render_template
 from flask_socketio import SocketIO, send
-
-app = Flask(__name__, static_folder = './build', static_url_path = '/')
+APP_DIR = os.path.abspath(os.path.dirname(__file__))
+STATIC_FOLDER = os.path.join(APP_DIR, '/build') # Where your webpack build output folder is
+TEMPLATE_FOLDER = os.path.join(APP_DIR, '/build') # Where your index.html file is located
+app = Flask(__name__, static_folder=STATIC_FOLDER, template_folder=TEMPLATE_FOLDER, static_url_path='/')
+app.debug = False
 app.config['SECRET_KEY'] = 'mysecret'
 
 socketIo = SocketIO(app, cors_allowed_origins="*")
